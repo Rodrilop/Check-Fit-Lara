8b86aeeeed27e21d789bc2cc8c02958e5873a5c5
rodrilop <64019127+Rodrilop@users.noreply.github.com>
2022-06-02 15:22:51 -0300

    Ajuste Layout Index dieta e dietaalimento

--- resources/views/dieta/index.blade.php
+++ resources/views/dieta/index.blade.php
@@ -1,24 +1,77 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Tabela de Dietas
+        </h2>
+    </x-slot>
+
     <div>
-        <div class="block mb-8">
-            <a href="{{ route('dieta.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">Criar Dieta</a>
+        <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="block mb-8">
+                <a href="{{ route('dieta.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">Cadastrar Dieta</a>
+            </div>
+            <div class="flex flex-col">
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200 w-full">
+                                <thead>
+                                <tr>
+                                    <th scope="col" width="50" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        ID
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Nome
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Calorias
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Data de Início
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Data de Término
+                                    </th>
+                                    <th scope="col" width="200" class="px-6 py-3 bg-gray-50">
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                @foreach ($dados as $d)
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                            {{ $d->id }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                            {{ $d->nm_dieta }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                            {{ $d->qt_caloria_dieta }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                            {{ $d->dt_inicio_dieta }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                                            {{ $d->dt_termino_dieta }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                                            <a href="{{ route('dieta.show', $d->id) }}" class="text-blue-600 hover:text-blue-900 mb-2 mr-2">Mostrar</a>
+                                            <a href="{{ route('dieta.edit', $d->id) }}" class="text-indigo-600 hover:text-indigo-900 mb-2 mr-2">Editar</a>
+                                            <form class="inline-block" action="{{ route('dieta.destroy', $d->id) }}" method="POST" onsubmit="return confirm('Tem certeza que deseja excluir?');">
+                                                <input type="hidden" name="_method" value="DELETE">
+                                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                                <input type="submit" class="text-red-600 hover:text-red-900 mb-2 mr-2" value="Deletar">
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
-        <table>
-            <tr>
-                <th>Nome</th>
-                <th>Calorias</th>
-                <th>Data de Inicio</th>
-                <th>Data de Termino</th>
-            </tr>
-            @foreach($dados as $d)
-            <tr>
-                <td>{{$d->nm_dieta}}</td>
-                <td>{{$d->qt_caloria_dieta}}</td>
-                <td>{{$d->dt_inicio_dieta}}</td>
-                <td>{{$d->dt_termino_dieta}}</td>
-            </tr>
-            @endforeach
-        </table>
-    </div>    
-    
+    </div>
 </x-app-layout>
